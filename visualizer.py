1dde109f71c5a85188aae111a626543ad2d80cfc
Weng Lam Cheung <zywenglam1024@gmail.com>
2019-11-02 13:05:16 +0800

    change it back to 256

--- visualizer.py
+++ visualizer.py
@@ -6,22 +6,26 @@ import sys
 # This import registers the 3D projection, but is otherwise unused.
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
-RESO = 30
-RATIO = 3
+RESO = 40
+RATIO = 1
 TOTAL = RESO * RATIO
-iteration = 0
 
 #
 # OPEN THE TARGET PNG
 #
-img = Image.open('result/output_0.png')
+img = Image.open('dataSet/output_5.png')
 data = np.array(img)
 
 #
 # CONVERT THE 2D PNG TO ARRAY THAT CAN BE REPRESENT BY VOXEL
 #
 data = np.resize(data, (TOTAL, TOTAL, TOTAL, 3))
-data = data[::3, ::3, ::3, 0:]
+
+#  -----------------------------------------
+#    UNCOMMENT IT IF YOU HAVE ENLARGED
+#  -----------------------------------------
+# data = data[::3, ::3, ::3, 0:]
+
 colors = np.divide(data, 255)
 
 cube = np.zeros((RESO, RESO, RESO), dtype=bool)
@@ -39,5 +43,5 @@ for i in range(colors.shape[0]):
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.voxels(cube, facecolors=colors, edgecolor='grey')
-plt.savefig('voxel_' + str(iteration) + '.png')
+plt.savefig('voxel_' + str(5) + '.png')
 plt.show()
