ee9e2e4591117945d39fee5d4e1fa8bcd609aec3
Weng Lam Cheung <zywenglam1024@gmail.com>
2019-11-02 16:24:02 +0800

    add a 255 version in visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -11,42 +11,61 @@ RATIO = 1
 TOTAL = RESO * RATIO
 
 #
-# OPEN THE TARGET PNG
+#  OPEN AND CONVERT 2D PNG TO 3D VOXEL ARRAY
 #
 img = Image.open('dataSet/output_5.png')
 data = np.array(img)
-#
-# CONVERT THE 2D PNG TO ARRAY THAT CAN BE REPRESENT BY VOXEL
-#
 data = np.resize(data, (TOTAL, TOTAL, TOTAL, 3))
-
 #  -----------------------------------------
 #    UNCOMMENT IT IF YOU HAVE ENLARGED
 #  -----------------------------------------
-# data = data[::3, ::3, ::3, 0:]
+# data = data[::RATIO, ::RATIO, ::RATIO, 0:]
 
-colors = np.divide(data, 255)
+#
+#  CHECKING FROM 128 TO 192 LEVEL
+#
 cube = np.zeros((RESO, RESO, RESO), dtype=bool)
+for CutOff in range(128, 192):
+    transparent = np.array([CutOff, CutOff, CutOff])
 
-for count in range(101):
-    #
-    # CHANGING FROM 1 TO 100 LEVEL
-    #
-    transparent = np.array([0.0 + count * 0.01, 0.0 + count * 0.01, 0.0 + count * 0.01])
-    for i in range(colors.shape[0]):
-        for j in range(colors.shape[1]):
-            for k in range(colors.shape[2]):
-                if colors[i][j][k][0] < transparent[0] and colors[i][j][k][1] < transparent[1] and \
-                        colors[i][j][k][2] < transparent[2]:
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            for k in range(data.shape[2]):
+                # it is TRUE when all value in the compare result array is TRUE
+                if np.all(data[i][j][k] <= transparent):
                     cube[i][j][k] = True
 
+    colors = np.divide(data, 255)
+
     #
-    # PLOTTING THE GRAPH TO SEE THE VOXEL
+    #  PLOTTING GRAPH TO SEE VOXEL
     #
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     ax.voxels(cube, facecolors=colors, edgecolor='grey')
-    plt.savefig('ViewResult/view_' + str(count) + '.png')
-    # plt.show()
 
+    if not os.path.exists('ViewResult'):
+        os.makedirs('ViewResult')
+    plt.savefig('ViewResult/view_' + str(CutOff) + '.png')
     cube = np.zeros((RESO, RESO, RESO), dtype=bool)     # resetting the whole cube array
+
+
+# for count in range(101):
+#     transparent = np.array([0.0 + count * 0.01, 0.0 + count * 0.01, 0.0 + count * 0.01])
+#     for i in range(colors.shape[0]):
+#         for j in range(colors.shape[1]):
+#             for k in range(colors.shape[2]):
+#                 if colors[i][j][k][0] < transparent[0] and colors[i][j][k][1] < transparent[1] and \
+#                         colors[i][j][k][2] < transparent[2]:
+#                     cube[i][j][k] = True
+#
+#     #
+#     #  PLOTTING GRAPH TO SEE VOXEL
+#     #
+#     fig = plt.figure()
+#     ax = fig.gca(projection='3d')
+#     ax.voxels(cube, facecolors=colors, edgecolor='grey')
+#     plt.savefig('ViewResult/view_' + str(count) + '.png')
+#     # plt.show()
+#
+#     cube = np.zeros((RESO, RESO, RESO), dtype=bool)     # resetting the whole cube array
